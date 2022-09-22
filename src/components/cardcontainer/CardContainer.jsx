e153bdc4dff524aae6c9356bb1db9777b7a893c5
Guillaume Crégut <guillaume-cregut_student2021@wilder.school>
2022-09-22 09:53:33 +0200

    useEffect and axios for API fetch

--- src/components/cardcontainer/CardContainer.jsx
+++ src/components/cardcontainer/CardContainer.jsx
@@ -1,18 +1,34 @@
-import React,{useState} from 'react';
+import React,{useState,useEffect} from 'react';
 import Card from '../card/Card';
-
-import pirates from '../../datas/data';
+import axios from "axios";
 import './CardContainer.css';
 import Filter from '../filter/Filter';
 
 const CardContainer = () => {
     const [sex, setSex]=useState('');
+    const [pirateList, setPiratList]=useState([]);
+
+    useEffect(()=>{
+        const getPirates=async ()=>{
+            const url=`${process.env.REACT_APP_API_URL}/pirates/`;
+            await axios
+            .get(url)
+            .then((res)=>{
+                setPiratList(res.data);
+            })
+            .catch((err)=>{
+                console.log(err);
+            })
+        };
+        getPirates();
+    },[]);
+    
     return (
         <div className="Container">
             <h2>Liste des principaux pirates</h2>
             <Filter sex={sex} setSex={setSex} />
             <div className='CardContainer'>
-                {pirates.filter((item)=>item.sex===sex||sex==="").map((pirate,index) => <Card {...pirate} key={index}/>)}
+                {pirateList.filter((item)=>item.sex===sex||sex==="").map((pirate,index) => <Card {...pirate} key={index}/>)}
             </div>
         </div>
     )
